bd450e2e92fa25a7fb4ddf794cacd758f968464b
EMMYT555 <130493792+EMMYT555@users.noreply.github.com>
2025-11-12 13:41:24 -0600

    actualiacion de BodyComponents.kt

--- app/src/main/java/com/example/apiretro/component/BodyComponents.kt
+++ app/src/main/java/com/example/apiretro/component/BodyComponents.kt
@@ -1,7 +1,12 @@
 package com.example.apiretro.component
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -9,8 +14,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.apiretro.model.GameList
 import com.example.apiretro.utils.Constants.Companion.CUSTOM_BLACK
 
@@ -52,5 +60,12 @@ fun MainTopBar(title:String,showBackButton:Boolean = false,
 
 @Composable
 fun CardGame(game: GameList, onClick:()->Unit){
-
+    Card(
+        shape= RoundedCornerShape(5.dp),
+        modifier = Modifier.padding(3.dp).shadow(40.dp).clickable{onClick()}
+    ){
+        Column(){
+            //imagen
+        }
+    }
 }
